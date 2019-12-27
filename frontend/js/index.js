66797fcbae9607e3680adea974c720a608bb4c2a
Kunal Malhotra <KunalMalhotra@abcs-MacBook-Air-2.local>
2019-12-27 18:39:45 +0400

    Frontend completed 60%. HTML code done, javascript 80% and css need some in the alignment part

---
+++ frontend/js/index.js
@@ -0,0 +1,37 @@
+function validateName(inputFullName){
+    if (length(inputFullName) == 0){
+        alert("Please Enter A Valid Name");
+        return false;
+    }else{  
+        return true;
+    }
+}
+
+function validateEmailAddress(inputEmailAddress){
+    var email_format = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var validationString = "ibm.com";
+    if (inputEmailAddress.match(email_format)) {
+        if (inputEmailAddress.includes(validationString)) {
+            return true;
+        }
+    }else{
+        alert("Please enter a valid Email Address");
+        return false;
+    }
+}
+
+function validateGaia(inputGaia){
+
+}
+
+function requestdescription(inputRequestDescription){
+
+}
+
+function sumbitform(){
+    var fullname = document.getElementById("fullnameinput").value;
+    var emailAddress = document.getElementById("emailaddressinput").value;
+    var cloudpakvalue = document.getElementById("cloudpakinput").value;
+    var gaia = document.getElementById("gaiainput").value;
+    var requestdescription = document.getElementById("requestdescriptioninput").value;
+}

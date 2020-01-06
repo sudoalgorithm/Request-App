ea613274b5fc66e4e6e0300160bc1087ab633b93
Kunal Malhotra <kunalmalhotra@Kunals-MacBook-Pro.local>
2020-01-06 23:20:29 +0400

    Integrations complete

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,33 +1,44 @@
 import os
-from flask import Flask
-from flask_restful import Api, Resource
 import slack
 import certifi
 import ssl
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
 
+CORS(app, support_credentials=True)
+
 ssl_context = ssl.create_default_context(cafile=certifi.where())
+
 SLACK_API_TOKEN = os.environ['SLACK_BOT_TOKEN']
 
 client = slack.WebClient(token=SLACK_API_TOKEN, ssl=ssl_context)
 
-def send_message_slack(fullname_arg: str, emailaddress_arg: str, gaia_arg: str, op_arg: str ):
+def send_message_slack(fullname_arg: str, emailaddress_arg: str, cloud_pak_arg: str, gaia_arg: str, op_arg: str ):
     data = {
             'Full Name':fullname_arg,
             'Email Address':emailaddress_arg,
+            'Cloud Pak':cloud_pak_arg,
             'GAIA':gaia_arg,
             'Opportunity Description':op_arg
         }
-    client.chat_postMessage(
-        channel="website-app",
-        text=data
-    )
+    client.chat_postMessage(channel="website-app",text=data)
 
 class SendMessage(Resource):
-    def get(self, fullname_input, emailaddress_input, gaia_input, op_input):
-        send_message_slack(fullname_input, emailaddress_input, gaia_input, op_input)
-        return "OK"
+    def post(self):
+        data = request.json
+        fullname: str = data['fullname']
+        emailaddress: str = data['emailaddress']
+        cloud_pak: str = data['cloudpakvalue']
+        gaia: set = data['gaia']
+        opportunity_description: str = data['requestdescription']
+        try:
+            send_message_slack(fullname, emailaddress, cloud_pak, gaia, opportunity_description)
+            return True
+        except Exception as identifier:
+            return identifier
 
-api.add_resource(SendMessage,'/api/v1/sendmessage/<string:fullname_input>/<string:emailaddress_input>/<string:gaia_input>/<string:op_input>')
+api.add_resource(SendMessage,'/api/v1/sendmessage/')
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,9 +1,9 @@
 function validateName(inputFullName){
-    if (length(inputFullName) == 0){
+    if (inputFullName.length != 0){
+        return true;
+    }else{  
         alert("Please Enter A Valid Name");
         return false;
-    }else{  
-        return true;
     }
 }
 
@@ -20,16 +20,61 @@ function validateEmailAddress(inputEmailAddress){
     }
 }
 
-function validateGaia(inputGaia){
+function validateCloudPakValue(inputCloudPakValue){
+    if(inputCloudPakValue.length != 0){
+        return true;
+    }else{
+        alert("Please Enter A Cloud Pak Value");
+        return false;
+    }
 }
 
-function requestdescription(inputRequestDescription){
+function validateGaia(inputGaia){
+    if(inputGaia.length != 0){
+        return true;
+    }else{
+        alert("Please Enter A Valid Gaia");
+        return false;
+    }
+}
+
+function validateRequestdescription(inputRequestDescription){
+    if(inputRequestDescription.length != 0){
+        return true;
+    }else{
+        alert("Please Enter A Valid Gaia");
+        return false;
+    }
 }
 
 function sumbitform(){
+
     var fullname = document.getElementById("fullnameinput").value;
     var emailAddress = document.getElementById("emailaddressinput").value;
     var cloudpakvalue = document.getElementById("cloudpakinput").value;
     var gaia = document.getElementById("gaiainput").value;
     var requestdescription = document.getElementById("requestdescriptioninput").value;
+
+    if(validateName(fullname) && validateEmailAddress(emailAddress) && validateCloudPakValue(cloudpakvalue) && validateGaia(gaia) && validateRequestdescription(requestdescription)){
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "http://0.0.0.0:8000/api/v1/sendmessage/", true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+            }
+        };
+        data = JSON.stringify({
+            'fullname': fullname,
+            'emailaddress': emailAddress,
+            'cloudpakvalue': cloudpakvalue,
+            'gaia':gaia,
+            'requestdescription':requestdescription
+        })
+        xhr.send(data);
+        return true;
+    }else{
+        alert("Something went wrong, Please check your inputs");
+        return false;
+    }
+    
 }
